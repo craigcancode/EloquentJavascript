fa5c7d822f1a292d46ee6ae73011550885254b66
Roger Wilson <rcw271828@users.noreply.github.com>
2018-02-09 23:54:39 -0500

    Added solution for Chapter-3, Minimum

---
+++ Chapter-3/Minimum/Minimum.js
@@ -0,0 +1,10 @@
+/*
+Write a function min that takes two arguments and returns their minimum.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+*/
+
+let min = (a, b) => { return (a < b) ? a : b; }
